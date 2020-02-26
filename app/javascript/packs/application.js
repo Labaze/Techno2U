333a844dc9a5fad3b5f35389d3e1f91f0ec41620
Marc Combarel <marc.combarel@edhec.com>
2020-02-26 16:42:58 +0100

    navbar for Party index + a lot of JS

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,3 +2,7 @@ import "bootstrap";
 import "../plugins/blink_text_footer";
 import "../plugins/like_color_change";
 import "../plugins/geolocalisation";
+import "../plugins/sidenav_opening";
+import "../plugins/today_date";
+import "../plugins/calendar";
+
