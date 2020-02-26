2126617b159ad7ef25411d800967da2b08075bef
Pierre de Bilbao <pierre.debilbao@gmail.com>
2020-02-26 11:01:20 +0100

    seed start + correction type

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'nokogiri'
+require 'open-uri'
+require 'CSV'
+
+puts 'Destroy all the database'
+
+Party.destroy_all
+User.destroy_all
+Attending.destroy_all
+Artist.destroy_all
+Lineup.destroy_all
+Genre.destroy_all
+Preference.destroy_all
+
+puts 'Database clean'
+
+# Seeding Genres
+puts 'Creating genres'
+
+url = "https://en.wikipedia.org/wiki/List_of_electronic_music_genres"
+list_file = open(url).read
+list_doc = Nokogiri::HTML(list_file)
+
+list_doc.search
+Genre.create(name: 'House')
+
+puts 'Genres created'
