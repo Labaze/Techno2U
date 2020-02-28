39b339cd0556cfbbcbf47a03bc233f5cce65b991
Marc Combarel <marc.combarel@edhec.com>
2020-02-28 17:15:09 +0100

    updated the quizz cards

--- app/controllers/preferences_controller.rb
+++ app/controllers/preferences_controller.rb
@@ -1,18 +1,16 @@
 require_relative '../services/soundcloud'
 
-
 class PreferencesController < ApplicationController
   # skip_before_action :authenticate_user!, only: [:home]
 
   def new
     @preference = Attending.new
     authorize @preference
-    artists = Artist.all
-    # artists = ['maceo plex', 'mall grab']
+    artists = Artist.all.sample(5)
     @user = current_user
 
     @soundcloud_artist_ids = []
-    @tracks = []
+    track_ids = []
 
     artists.each do |artist|
       @soundcloud_artist_ids << SoundCloud.new(name: artist.name)
@@ -20,9 +18,11 @@ class PreferencesController < ApplicationController
 
     @soundcloud_artist_ids.each do |soundcloud_artist_id|
       unless soundcloud_artist_id.nil?
-        @tracks << soundcloud_artist_id.sound_id
+        track_ids << soundcloud_artist_id.sound_id
       end
     end
+
+    @tracks = track_ids.reject { |c| c == "" }
   end
 
   def create
