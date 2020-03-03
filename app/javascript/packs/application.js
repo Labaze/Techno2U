7715e447adc3b44776350668f2a1de98c376a6a9
Marc Combarel <marc.combarel@edhec.com>
2020-03-03 14:42:07 +0100

    added the tuto for the first session

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,13 +1,14 @@
-import "bootstrap";
-import "../plugins/blink_text_footer";
-import "../plugins/like_deslike_color_change";
-import "../plugins/geolocalisation";
-import "../plugins/sidenav_opening";
-import "../plugins/today_date";
-import "../plugins/calendar";
-import "../plugins/audio_quizz_card_animation";
-import "../plugins/soundcloud_player_according_to_select";
-import "../plugins/dashboard_page_switch";
+// import "bootstrap";
+// import "../plugins/blink_text_footer";
+// import "../plugins/like_deslike_color_change";
+// import "../plugins/geolocalisation";
+// import "../plugins/sidenav_opening";
+// import "../plugins/today_date";
+// import "../plugins/calendar";
+// import "../plugins/audio_quizz_card_animation";
+// import "../plugins/soundcloud_player_according_to_select";
+// import "../plugins/dashboard_page_switch";
+import "../plugins/first_session_animation";
 
 // Select2
 import "select2/dist/css/select2.css";
@@ -19,3 +20,4 @@ import 'mapbox-gl/dist/mapbox-gl.css';
 import { initMapbox } from '../plugins/init_mapbox';
 
 initMapbox();
+
