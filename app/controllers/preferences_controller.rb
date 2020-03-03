17f3d9b1e2da7fdaa5e0355d7ecd6cf409bd3542
Marc Combarel <marc.combarel@edhec.com>
2020-03-03 16:16:51 +0100

    quizz back at work

--- app/controllers/preferences_controller.rb
+++ app/controllers/preferences_controller.rb
@@ -6,22 +6,14 @@ class PreferencesController < ApplicationController
   def new
     @preference = Attending.new
     authorize @preference
-    artists = Artist.all.sample(5)
+    @artists = Artist.all.sample(5)
     @user = current_user
-
-    @soundcloud_artist_ids = []
     track_ids = []
 
-    artists.each do |artist|
-      @soundcloud_artist_ids << SoundCloud.new(name: artist.name)
-    end
 
-    @soundcloud_artist_ids.each do |soundcloud_artist_id|
-      unless soundcloud_artist_id.nil?
-        track_ids << soundcloud_artist_id.sound_id
-      end
+    @artists.each do |artist|
+      track_ids << artist.track_url
     end
-
     @tracks = track_ids.reject { |c| c == "" }
   end
 
