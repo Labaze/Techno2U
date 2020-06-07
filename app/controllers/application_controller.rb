498c7a9ad536691f5038931cd809bc6ac0a82d37
Marc Combarel <marc.combarel@edhec.com>
2020-06-07 18:16:34 +0200

    added attribute to devise: Age, Gendre, Name

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,7 +17,7 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :admin] )
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :age, :gender, :admin] )
     # For additional in app/views/devise/registrations/edit.html.erb
     devise_parameter_sanitizer.permit(:account_update, keys: [:name, :admin] )
   end
