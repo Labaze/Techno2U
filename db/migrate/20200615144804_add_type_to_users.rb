96679081d59635569f43c1c460d8d8d4d06333f2
Marc Combarel <marc.combarel@edhec.com>
2020-06-17 18:17:26 +0200

    changed migrationfile

--- db/migrate/20200615144804_add_type_to_users.rb
+++ db/migrate/20200615144804_add_type_to_users.rb
@@ -1,5 +1,5 @@
-class AddTypeToUsers < ActiveRecord::Migration[5.2]
+class AddUserCategoryToUsers < ActiveRecord::Migration[5.2]
   def change
-    add_column :users, :cluster, :string
+    add_column :users, :user_category, :string
   end
 end
